8572ce230b8f8c4030596bf7172adc9fc43e9062
MysticCode3 <83873931+MysticCode3@users.noreply.github.com>
2024-11-29 20:31:47 -0500

    Update ArcadeDriveSubsystem.java

--- ArcadeDriveSubsystem.java
+++ ArcadeDriveSubsystem.java
@@ -13,21 +13,17 @@ public class ArcadeDriveSubsystem extends SubsystemBase {
     private final DifferentialDrive drive;
 
     public ArcadeDriveSubsystem() {
-        // Initialize motor controllers
         LeftFrontMotor = new CANSparkMax(0, MotorType.kBrushless);
         LeftBackMotor = new CANSparkMax(1, MotorType.kBrushless);
         RightFrontMotor = new CANSparkMax(2, MotorType.kBrushless);
         RightBackMotor = new CANSparkMax(3, MotorType.kBrushless);
 
-        // Make front motors fallow back motor
         LeftFrontMotor.follow(LeftBackMotor);
         RightFrontMotor.follow(RightBackMotor);
 
-        // Create DifferentialDrive
         drive = new DifferentialDrive(LeftFrontMotor, RightFrontMotor);
     }
 
-    // Method to drive
     public void arcadeDrive(double speed, double rotation) {
         drive.arcadeDrive(speed, rotation);
     }
